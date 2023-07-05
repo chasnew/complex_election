9ff3d291480566f0a47d88cdf99d06973c10b3a4
chasnew <chanuwas.asw@gmail.com>
2023-07-05 11:09:38 -0700

    Added candidate class

--- agent.py
+++ agent.py
@@ -1,18 +1,20 @@
-class Agent:
+class Resident:
     """
-    Represents a single person with an ID and the person's opinon or policy position.
+    Represents a resident with an ID and the person's opinion or policy position.
     """
 
-    def __init__(self, id_: str, x):
+    def __init__(self, id_, d_id, x):
         """
         Initializes the agent.
 
         Parameters
         ----------
-        id_ : str
-            The ID of the agent.
+        id_: The ID of the agent.
+        d_id: district ID of the agent
+        x: opinion value or policy preference of the agent
         """
         self.id = id_
+        self.d_id = d_id
         self.x = x
 
     def __repr__(self):
@@ -20,3 +22,30 @@ class Agent:
         Text representation of the agent.
         """
         return f'Agent ({self.id}|{self.x})'
+
+
+
+class Candidate(Resident):
+    """
+    Represents a nominated political candidate with an assigned party and status of whether they got elected
+    """
+
+    def __init__(self, id_, d_id, x, party_id):
+        """
+        Initializes the agent.
+
+        Parameters
+        ----------
+        id_: The ID of the agent.
+        d_id: district ID of the agent
+        x: opinion value or policy preference of the agent
+        """
+        super().__init__(id_, d_id, x)
+        self.party_id = party_id
+        self.elected = False
+
+    def __repr__(self):
+        """
+        Text representation of the agent.
+        """
+        return f'Candidate ({self.id}|{self.x}|{self.party_id})'

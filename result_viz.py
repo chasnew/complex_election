ff6386779779ad8cdce9721367052145da8206dd
chasnew <chanuwas.asw@gmail.com>
2023-08-22 15:52:16 -0700

    More condition comparisons

--- result_viz.py
+++ result_viz.py
@@ -18,26 +18,59 @@ result_df = pd.concat(result_list).reset_index(drop=True)
 det_results = result_df[result_df['voting'] == 'deterministic']
 det_results['rep_dist_num'] = det_results['rep_num'].astype(str) + '_' + det_results['district_num'].astype(str)
 det_results['party_num'] = det_results['party_num'].astype(str)
-sns.swarmplot(det_results, x='rep_dist_num', y='js_distance', hue='party_num')
+sns.stripplot(det_results, x='rep_dist_num', y='js_distance', hue='party_num')
 plt.show()
 
 major_results = result_df[result_df['voting'] == 'one_per_party']
 major_results['party_num'] = major_results['party_num'].astype(str)
-sns.swarmplot(major_results, x='district_num', y='js_distance', hue='party_num')
+sns.stripplot(major_results, x='district_num', y='js_distance', hue='party_num')
 plt.show()
 
 propr_results = result_df[result_df['voting'] == 'proportional_rep']
 propr_results['rep_dist_num'] = propr_results['rep_num'].astype(str) + '_' + propr_results['district_num'].astype(str)
 propr_results['party_num'] = propr_results['party_num'].astype(str)
-sns.swarmplot(propr_results, x='rep_dist_num', y='js_distance', hue='party_num')
+sns.stripplot(propr_results, x='rep_dist_num', y='js_distance', hue='party_num')
 plt.show()
 
 
+# Contrasting Plurality voting to Proportional representation (rep * district = 10)
 cond1 = (result_df['voting'] == 'deterministic') & (result_df['district_num'] == 10) &\
         (result_df['rep_num'] == 1) & (result_df['party_num'].isin([2,5,10]))
-cond2 = (result_df['voting'] == 'one_per_party') & (result_df['district_num'] == 10)
-cond3 = (result_df['voting'] == 'proportional_rep') & (result_df['district_num'] == 1) &\
+cond2 = (result_df['voting'] == 'deterministic') & (result_df['district_num'] == 1) &\
+        (result_df['rep_num'] == 10) & (result_df['party_num'].isin([2,5,10]))
+cond3 = (result_df['voting'] == 'one_per_party') & (result_df['district_num'] == 10)
+cond4 = (result_df['voting'] == 'proportional_rep') & (result_df['district_num'] == 1) &\
         (result_df['rep_num'] == 10)
 
-sns.swarmplot(result_df[cond1 | cond2 | cond3], x='voting', y='js_distance', hue='party_num')
+comb1_results = result_df[cond1 | cond2 | cond3]
+comb1_results['voting'] = comb1_results['voting'] + '_' + comb1_results['rep_num'].astype(str) +\
+                          '_' + comb1_results['district_num'].astype(str)
+comb1_results['party_num'] = comb1_results['party_num'].astype(str)
+sns.stripplot(comb1_results, x='voting', y='js_distance', hue='party_num')
+plt.show()
+
+
+
+# Contrasting Plurality voting to Proportional representation (rep * district = 50)
+cond1 = (result_df['voting'] == 'one_per_party') & (result_df['district_num'] == 50)
+cond2 = (result_df['voting'] == 'proportional_rep') & (result_df['district_num'] == 5) &\
+        (result_df['rep_num'] == 10)
+
+comb2_results = result_df[cond1 | cond2]
+comb2_results['party_num'] = comb2_results['party_num'].astype(str)
+sns.stripplot(comb2_results, x='voting', y='js_distance', hue='party_num')
+plt.show()
+
+
+
+# Contrasting Plurality voting to Proportional representation (rep * district = 100)
+cond1 = (result_df['voting'] == 'one_per_party') & (result_df['district_num'] == 100)
+cond2 = (result_df['voting'] == 'proportional_rep') & (result_df['district_num'] == 10) &\
+        (result_df['rep_num'] == 10)
+cond3 = (result_df['voting'] == 'proportional_rep') & (result_df['district_num'] == 5) &\
+        (result_df['rep_num'] == 20)
+
+comb3_results = result_df[cond1 | cond2 | cond3]
+comb3_results['party_num'] = comb3_results['party_num'].astype(str)
+sns.stripplot(comb3_results, x='voting', y='js_distance', hue='party_num')
 plt.show()
